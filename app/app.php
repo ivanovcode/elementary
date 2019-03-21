53253db91a33e6293b091294995e97f6650c6a66
ivanov <ivanov@ptnl>
2019-03-21 18:41:45 +0300

    support multiphone

--- app/app.php
+++ app/app.php
@@ -52,6 +52,18 @@
             return true;
         }
     }
+
+    function shift_in_left (&$arr) {
+        $item = array_shift($arr);
+        array_push ($arr,$item);
+    }
+
+    function shift_in_right (&$arr) {
+        $item = array_pop($arr);
+        array_unshift ($arr,$item);
+    }
+
+
     if (!function_exists('write_ini_file')) {
 
         function write_ini_file($file, $array = []) {
@@ -114,12 +126,15 @@
         }
     }
 
-    ini_set('error_reporting', 0);
-    ini_set('display_errors', 0);
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    /*ini_set('error_reporting', 0);
+    ini_set('display_errors', 0);*/
 
     $config = parse_ini_file(dirname(__FILE__).'/app.ini', true);
 
-    if($_GET['m']=='admin') {
+    if(isset($_GET['m']) && $_GET['m']=='admin') {
         if($config['admin']['email']!='demo@demo.com') {
             session_start();
             if(!empty($_SESSION['user_session']) && $_SESSION['user_session'] == $config['admin']['session']) {
@@ -146,7 +161,7 @@
         die();
     }
 
-    if($_GET['m']=='addphone') {
+    if(isset($_GET['m']) && $_GET['m']=='addphone') {
             session_start();
             if(!empty($_SESSION['user_session']) && $_SESSION['user_session'] == $config['admin']['session']) {
                 $_POST['phone'] = preg_replace("/[^0-9]/", "", $_POST['phone']);
@@ -163,7 +178,7 @@
             }
     }
 
-    if($_GET['m']=='delphone') {
+    if(isset($_GET['m']) && $_GET['m']=='delphone') {
         session_start();
         if(!empty($_SESSION['user_session']) && $_SESSION['user_session'] == $config['admin']['session']) {
             $_POST['phone'] = preg_replace("/[^0-9]/", "", $_POST['phone']);
@@ -191,7 +206,7 @@
         }
     }
 
-    if($_GET['m']=='signout') {
+    if(isset($_GET['m']) && $_GET['m']=='signout') {
         session_start();
         unset($_SESSION['user_session']);
         if(session_destroy()) {
@@ -203,7 +218,7 @@
         }
     }
 
-    if($_GET['m']=='signin') {
+    if(isset($_GET['m']) && $_GET['m']=='signin') {
         if (!(isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])
             && $_SERVER['PHP_AUTH_USER'] == $config['admin']['email']
             && md5($_SERVER['PHP_AUTH_PW']) == $config['admin']['password'])) {
@@ -218,8 +233,20 @@
     }
 
     $current = intval($config['cache']['phone']);
+
+
     $current = ($current+1>count($config['phones'])?1:$current+1);
     $config['cache']['phone'] = $current;
-    $phone = $config['phones'][$current];
-    if($_GET['m']=="echo")echo $phone;
+
+
+    $key = array_search($config['phones'][$current], $config['phones']);
+    $phones = $config['phones'];
+    shift_in_right($phones);
+    shift_in_left($phones);
+    $x=0; if($key>1) while ($x++<$key-1) shift_in_right($phones);
+    array_combine(range(1, count($phones)), $phones);
+
+    $phone = $phones[1];
+
+    if(isset($_GET['m']) && $_GET['m']=="echo") echo $phone;
     write_ini_file(dirname(__FILE__).'/app.ini', $config);
